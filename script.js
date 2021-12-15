cfec537aee5d8a44fcfa3e96120d0d1872c7bb3d
Kern84 <adrienleroy1984@gmail.com>
2021-12-15 17:03:15 +0100

    progression

--- script.js
+++ script.js
@@ -22,9 +22,25 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
         .then(data => mod.textContent = "Titre : " + data["results"][0]["title"] + " ; Genre : " + data["results"][0]["genres"] + " ; Date de sortie : " + data["results"][0]["date_published"] + " ; Rated : " + data["results"][0]["rated"] + " ; Score IMDB : " + data["results"][0]["imdb_score"] + " ; Réalisateur : " + data["results"][0]["directors"] + " ; Acteurs : " + data["results"][0]["actors"] + " ; Durée : " + data["results"][0]["duration"] + " ; Pays : " + data["results"][0]["countries"] + " ; Résultat Box-Office : " + data["results"][0]["worldwide_gross_income"] + " ; Résumé du film : " + data["results"][0]["description"]);
 
 
-/* Catégorie */
-let image1 = document.getElementById("image_meilleur_film_1");
-fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=biography")
-        .then(response => response.json())
-        .then(data => image1.src = data["results"][0]["image_url"]);
+/* Catégorie les meilleurs films */
 
+
+let image1 = document.getElementById("image_meilleur_film_1");
+let titre1 = document.getElementById("titre_meilleur_film_1");
+let mod1 = document.getElementById("meilleur_film_modal_1");
+
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
+        .then(response => response.json())
+        .then(data => image1.src = data["results"][1]["image_url"]);
+
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
+        .then(response => response.json())
+        .then(data => titre1.textContent = data["results"][1]["title"]);
+
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
+        .then(response => response.json())
+        .then(data => mod1.textContent = "Titre : " + data["results"][1]["title"] + " ; Genre : " + data["results"][1]["genres"] + " ; Date de sortie : " + data["results"][1]["date_published"] + " ; Rated : " + data["results"][1]["rated"] + " ; Score IMDB : " + data["results"][1]["imdb_score"] + " ; Réalisateur : " + data["results"][1]["directors"] + " ; Acteurs : " + data["results"][1]["actors"] + " ; Durée : " + data["results"][1]["duration"] + " ; Pays : " + data["results"][1]["countries"] + " ; Résultat Box-Office : " + data["results"][1]["worldwide_gross_income"] + " ; Résumé du film : " + data["results"][1]["description"]);
+
+
+/* Catégorie */
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=biography")
