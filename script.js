c7bf0134d47c7721842e0efe61f9877f5a39c1b4
Kern84 <adrienleroy1984@gmail.com>
2021-12-16 16:17:56 +0100

    Manque les modales pour les carousels

--- script.js
+++ script.js
@@ -19,28 +19,123 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
 
 fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
         .then(response => response.json())
-        .then(data => mod.textContent = "Titre : " + data["results"][0]["title"] + " ; Genre : " + data["results"][0]["genres"] + " ; Date de sortie : " + data["results"][0]["date_published"] + " ; Rated : " + data["results"][0]["rated"] + " ; Score IMDB : " + data["results"][0]["imdb_score"] + " ; Réalisateur : " + data["results"][0]["directors"] + " ; Acteurs : " + data["results"][0]["actors"] + " ; Durée : " + data["results"][0]["duration"] + " ; Pays : " + data["results"][0]["countries"] + " ; Résultat Box-Office : " + data["results"][0]["worldwide_gross_income"] + " ; Résumé du film : " + data["results"][0]["description"]);
+        .then(data => mod.textContent = "Titre : " + data["results"][0]["title"] + " ; Genre : " + data["results"][0]["genres"]
+        + " ; Date de sortie : " + data["results"][0]["date_published"] + " ; Rated : " + data["results"][0]["rated"]
+        + " ; Score IMDB : " + data["results"][0]["imdb_score"] + " ; Réalisateur : " + data["results"][0]["directors"]
+        + " ; Acteurs : " + data["results"][0]["actors"] + " ; Durée : " + data["results"][0]["duration"] + " ; Pays : " + data["results"][0]["countries"]
+        + " ; Résultat Box-Office : " + data["results"][0]["worldwide_gross_income"] + " $" + " ; Résumé du film : " + data["results"][0]["description"]);
 
 
-/* Catégorie les meilleurs films */
+/* Catégorie les meilleurs films carousel 1 */
+for (let i = 1; i < 8; i++) {
+
+    let localisation_image = document.getElementById("image_meilleur_film_" + i.toString());
+    let localisation_titre = document.getElementById("titre_meilleur_film_" + i.toString());
+    let localisation_modal = document.getElementById("meilleur_film_modal_" + i.toString());
+
+    if (i < 5) {
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
+            .then(response => response.json())
+            .then(data => localisation_image.src = data["results"][i]["image_url"]);
+
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
+                .then(response => response.json())
+                .then(data => localisation_titre.textContent = data["results"][i]["title"]);
+    } else {
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes")
+            .then(response => response.json())
+            .then(data => localisation_image.src = data["results"][(i - 5)]["image_url"]);
+
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes")
+            .then(response => response.json())
+            .then(data => localisation_titre.textContent = data["results"][(i - 5)]["title"]);
+    }
+/*
+    fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
+            .then(response => response.json())
+            .then(data => localisation_modal.textContent = "Titre : " + data["results"][i]["title"] + " ; Genre : " + data["results"][i]["genres"]
+            + " ; Date de sortie : " + data["results"][i]["date_published"] + " ; Rated : " + data["results"][i]["rated"]
+            + " ; Score IMDB : " + data["results"][i]["imdb_score"] + " ; Réalisateur : " + data["results"][i]["directors"]
+            + " ; Acteurs : " + data["results"][i]["actors"] + " ; Durée : " + data["results"][i]["duration"] + " ; Pays : " + data["results"][i]["countries"]
+            + " ; Résultat Box-Office : " + data["results"][i]["worldwide_gross_income"] + " ; Résumé du film : " + data["results"][i]["description"]);
+*/
+}
 
 
-let image1 = document.getElementById("image_meilleur_film_1");
-let titre1 = document.getElementById("titre_meilleur_film_1");
-let mod1 = document.getElementById("meilleur_film_modal_1");
+/* Catégorie 1  Biographie carousel 2 */
+for (let j = 0; j < 8; j++) {
 
-fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
-        .then(response => response.json())
-        .then(data => image1.src = data["results"][1]["image_url"]);
+    let localisation_image1 = document.getElementById("image_categorie1_" + j.toString());
+    let localisation_titre1 = document.getElementById("titre_categorie1_" + j.toString());
+    let localisation_modal1 = document.getElementById("categorie1_modal_" + j.toString());
 
-fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
-        .then(response => response.json())
-        .then(data => titre1.textContent = data["results"][1]["title"]);
+    if (j < 5) {
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=biography")
+            .then(response => response.json())
+            .then(data => localisation_image1.src = data["results"][j]["image_url"]);
 
-fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes")
-        .then(response => response.json())
-        .then(data => mod1.textContent = "Titre : " + data["results"][1]["title"] + " ; Genre : " + data["results"][1]["genres"] + " ; Date de sortie : " + data["results"][1]["date_published"] + " ; Rated : " + data["results"][1]["rated"] + " ; Score IMDB : " + data["results"][1]["imdb_score"] + " ; Réalisateur : " + data["results"][1]["directors"] + " ; Acteurs : " + data["results"][1]["actors"] + " ; Durée : " + data["results"][1]["duration"] + " ; Pays : " + data["results"][1]["countries"] + " ; Résultat Box-Office : " + data["results"][1]["worldwide_gross_income"] + " ; Résumé du film : " + data["results"][1]["description"]);
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=biography")
+                .then(response => response.json())
+                .then(data => localisation_titre1.textContent = data["results"][j]["title"]);
+    } else {
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes&genre=biography")
+            .then(response => response.json())
+            .then(data => localisation_image1.src = data["results"][(j - 5)]["image_url"]);
 
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes&genre=biography")
+            .then(response => response.json())
+            .then(data => localisation_titre1.textContent = data["results"][(j - 5)]["title"]);
+    }
+}
 
-/* Catégorie */
-fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=biography")
+/* Catégorie 2  Aventure carousel 3 */
+for (let k = 0; k < 8; k++) {
+
+    let localisation_image2 = document.getElementById("image_categorie2_" + k.toString());
+    let localisation_titre2 = document.getElementById("titre_categorie2_" + k.toString());
+    let localisation_modal2 = document.getElementById("categorie2_modal_" + k.toString());
+
+    if (k < 5) {
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=Adventure")
+            .then(response => response.json())
+            .then(data => localisation_image2.src = data["results"][k]["image_url"]);
+
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=Adventure")
+                .then(response => response.json())
+                .then(data => localisation_titre2.textContent = data["results"][k]["title"]);
+    } else {
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes&genre=Adventure")
+            .then(response => response.json())
+            .then(data => localisation_image2.src = data["results"][(k - 5)]["image_url"]);
+
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes&genre=Adventure")
+            .then(response => response.json())
+            .then(data => localisation_titre2.textContent = data["results"][(k - 5)]["title"]);
+    }
+}
+
+/* Catégorie 3  Animation carousel 4 */
+for (let l = 0; l < 8; l++) {
+
+    let localisation_image3 = document.getElementById("image_categorie3_" + l.toString());
+    let localisation_titre3 = document.getElementById("titre_categorie3_" + l.toString());
+    let localisation_modal3 = document.getElementById("categorie3_modal_" + l.toString());
+
+    if (l < 5) {
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=Animation")
+            .then(response => response.json())
+            .then(data => localisation_image3.src = data["results"][l]["image_url"]);
+
+        fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&genre=Animation")
+                .then(response => response.json())
+                .then(data => localisation_titre3.textContent = data["results"][l]["title"]);
+    } else {
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes&genre=Animation")
+            .then(response => response.json())
+            .then(data => localisation_image3.src = data["results"][(l - 5)]["image_url"]);
+
+        fetch("http://localhost:8000/api/v1/titles/?page=2&sort_by=-imdb_score&sort_by=-votes&genre=Animation")
+            .then(response => response.json())
+            .then(data => localisation_titre3.textContent = data["results"][(l - 5)]["title"]);
+    }
+}
